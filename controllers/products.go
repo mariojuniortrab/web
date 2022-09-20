d37d2f8c6eb561d8665a9f9fd8bcfc1a5498726d
Mário Júnior <mariojuniortrab@gmail.com>
2022-09-20 15:32:08 -0300

    add register form

--- controllers/products.go
+++ controllers/products.go
@@ -11,3 +11,7 @@ var templates = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "Index", models.ListAllProducts())
 }
+
+func New(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "New", nil)
+}
