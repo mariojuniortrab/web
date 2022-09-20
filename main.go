d825242ff3bfedd16f4f2e3b55c2b19aabb97e96
Mário Júnior <mariojuniortrab@gmail.com>
2022-09-20 14:59:29 -0300

    list all products from database

--- main.go
+++ main.go
@@ -1,29 +1,67 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func databasConnect() *sql.DB {
+	conn := "user=postgres dbname=teste2 password=q1w2e3r4 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conn)
+
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
 
 type Product struct {
 	Name, Description string
 	Price             float64
-	Amount            float64
+	Amount            int
 }
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	products := []Product{
-		{Name: "T-shirt", Description: "Blue, very beautiful", Price: 39, Amount: 10},
-		{"Shoes", "Confortable", 89, 3},
-		{"Fone", "working", 59, 2},
-		{"New Product", "Very Good", 1, 20},
+	db := databasConnect()
+
+	selectedProductsList, err := db.Query("select * from products")
+	if err != nil {
+		panic(err)
 	}
+
+	p := Product{}
+	products := []Product{}
+
+	for selectedProductsList.Next() {
+		var id, amount int
+		var name, description string
+		var price float64
+
+		err = selectedProductsList.Scan(&id, &name, &description, &amount, &price)
+		if err != nil {
+			panic(err)
+		}
+
+		p.Name = name
+		p.Description = description
+		p.Amount = amount
+		p.Price = price
+
+		products = append(products, p)
+
+	}
+
 	templates.ExecuteTemplate(w, "Index", products)
+	defer db.Close()
 }
